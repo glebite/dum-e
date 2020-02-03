68c29688cd8050604c2715708a86bb4c6a9d999e
Lazlo <glebite@gmail.com>
2020-02-02 21:15:01 -0500

    first checkin

---
+++ src/app.py
@@ -0,0 +1,15 @@
+"""
+app.py - intended to be renamed later
+"""
+import sys
+import logging
+import configparser
+from flask import Flask
+
+def main():
+    """
+    stub for now - main function to be called and drive the whole shebang
+    """
+
+if __name__ == "__main__":
+    main()
