66928f3ee68538a3708d5c0158dd843f1081882a
Lazlo <glebite@gmail.com>
2020-09-04 21:54:46 -0400

    Fleshing out the template...

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -1,4 +1,19 @@
+// arduino.ino
+//
+// dummy install/template for building remote-make arduino code to
+// be deployed on the arduino attached to the raspberry pi.
+//
+
+// includes
+
+// constants
+
+// globals
+
+// local functions
+
 void setup() {
+    Serial.begin(115200);    // go broke or go home
 }
 
 void loop() {
